14b95ade5a3b96ee21134c082200a603932074e1
miko <miko@MIKOPUTER>
2020-12-06 17:16:20 +0100

    fgdsewhjysgfbksf

--- CMakeProject1/CMakeProject1/main.cpp
+++ CMakeProject1/CMakeProject1/main.cpp
@@ -8,9 +8,9 @@ int main()
 {
 	
     cout << "Hello CMake." << endl;
-	return 0;
+	
 
 	Car furka(static_cast<std::string>("mercedes") , static_cast<std::string>("c350"), 5, 150, 69 );
-
+	return 0;
 
 }
